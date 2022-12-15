2bc1f700aa31d1c7686baf240ab5592252e3b416
Proyash Podder <proyash.podder@gmail.com>
2022-12-15 13:44:33 -0500

    Organize decoder code

--- decryptor.py
+++ decryptor.py
@@ -1,5 +1,7 @@
 from typing import Optional
+import ndn.utils
 from ndn.app import NDNApp
+from ndn.types import InterestNack, InterestTimeout, InterestCanceled, ValidationFailure
 #from ndn.encoding import Name, InterestParam, BinaryStr, FormalName, MetaInfo
 import logging
 from decryptionpolicy.run import DecryptionPolicy
@@ -10,6 +12,12 @@ from tlvmodels import *
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from encryption import *
+from ECIES import *
+from hashlib import sha256
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
+from Crypto.PublicKey import ECC
+from base64 import b64decode, b64encode
 
 
 logging.basicConfig(format='[{asctime}]{levelname}:{message}',
@@ -39,4 +47,160 @@ class Decryptor:
         for kekName in kekNames:
             res[kekName] = ckName+'/ENCRYPTED-BY'+kekName
         return res
+        
+        
+    def getPrivkey(self,id):
+        privKeyLoc = sha256(Name.to_bytes(id.default_key().name)).digest().hex() + '.privkey'
+        f = open('privkeys/'+privKeyLoc, "rb")
+        privKey = f.read()
+        return privKey
+        
+    async def decode(self,app,keychain,contentName,identity):
+        id = keychain.touch_identity(identity)
+        privKey = self.getPrivkey(id)
+
+        encryptedPayload, iv = await self.fetchContent(app,contentName)
+        
+        
+        ckName = '/Home/livingroom/camera/feed/1/CK' #need to figure out how to get the CK name (either included in the encrypted payload or same prefix
+        ckData = await self.fetchCKData(app,ckName)
+        kekNames = self.parseCKNames(ckData)
+        ckNames = self.buildCKName(ckName,kekNames)
+        
+        
+        for key,value in ckNames.items():
+            try:
+                encryptedCK = await self.fetchEncryptedCK(app,value)
+                    
+                key = Name.normalize(key)
+                
+                kdkName = Name.to_str(key[:-3])+'/KDK/ENCRYPTED-BY'+ identity
+                
+                
+                encryptedKDK = await self.fetchKDK(app,kdkName)
+                    
+                print(bytes(encryptedKDK))
+                
+                kdk = self.decryptKDK(encryptedKDK,privKey)
+
+                
+                ck = self.decryptCK(encryptedCK,kdk)
+                
+                txt = self.decodeContent(encryptedPayload,ck,iv)
+
+            except:
+                print('Something wrong')
+        
+        
+        
+        
+        
+    async def fetchContent(self,app,contentName):
+        try:
+            name = Name.from_str(contentName)
+            print(f'Sending Interest {Name.to_str(name)}, {InterestParam(must_be_fresh=True, lifetime=6000)}')
+            data_name, meta_info, encryptedContent = await app.express_interest(
+                name, must_be_fresh=True, can_be_prefix=False, lifetime=6000)
+                
+            print(encryptedContent)
+            encryptedModel = EncryptedContent.parse(encryptedContent)
+            #print(bytes(encryptedModel.inner.encryptedPayload),type(encryptedModel),bytes(encryptedModel.inner.initializationVector))
+            encryptedPayload = bytes(encryptedModel.inner.encryptedPayload)
+            iv = bytes(encryptedModel.inner.initializationVector)\
+            
+            return encryptedPayload,iv
+        
+        
+        except InterestNack as e:
+            print(f'Nacked with reason={e.reason}')
+        except InterestTimeout:
+            print(f'Timeout')
+        except InterestCanceled:
+            print(f'Canceled')
+        except ValidationFailure:
+            print(f'Data failed to validate')
+        
+        
+    async def fetchCKData(self,app,ckName):
+        try:
+            name = Name.from_str(ckName)
+            print(f'Sending Interest {Name.to_str(name)}, {InterestParam(must_be_fresh=True, lifetime=6000)}')
+            data_name, meta_info, ckData = await app.express_interest(
+                name, must_be_fresh=True, can_be_prefix=False, lifetime=6000)
+            
+            return ckData
+                
+                
+        except InterestNack as e:
+            print(f'Nacked with reason={e.reason}')
+        except InterestTimeout:
+            print(f'Timeout')
+        except InterestCanceled:
+            print(f'Canceled')
+        except ValidationFailure:
+            print(f'Data failed to validate')
+    
+    async def fetchKDK(self,app,kdkName):
+        try:
+            name = Name.from_str(kdkName)
+            print(f'Sending Interest {Name.to_str(name)}, {InterestParam(must_be_fresh=True, lifetime=6000)}')
+            data_name, meta_info, kdk = await app.express_interest(
+            name, must_be_fresh=False, can_be_prefix=False, lifetime=6000)
+            
+            return kdk
+    
+        except InterestNack as e:
+            print(f'Nacked with reason={e.reason}')
+        except InterestTimeout:
+            print(f'Timeout')
+        except InterestCanceled:
+            print(f'Canceled')
+        except ValidationFailure:
+            print(f'Data failed to validate')
+            
+    async def fetchEncryptedCK(self,app,value):
+        try:
+            encryptedCK = ''
+            name = Name.from_str(value)
+            print(f'Sending Interest {Name.to_str(name)}, {InterestParam(must_be_fresh=True, lifetime=6000)}')
+            data_name, meta_info, encryptedCK = await app.express_interest(
+                name, must_be_fresh=False, can_be_prefix=False, lifetime=6000)
+                
+            #if(encryptedCK):
+            print(encryptedCK)
+            return encryptedCK
+            
+        except InterestNack as e:
+            print(f'Nacked with reason={e.reason}')
+        except InterestTimeout:
+            print(f'Timeout')
+        except InterestCanceled:
+            print(f'Canceled')
+        except ValidationFailure:
+            print(f'Data failed to validate')
+            
+    def decryptKDK(self,encryptedKDK,privKey):
+        k = ECC.import_key(b64decode(privKey))
+        kdk = decrypt(k,bytes(encryptedKDK))
+        print(kdk)
+        return kdk
+        
+    def decryptCK(self,encryptedCK,kdk):
+        ck = decrypt(ECC.import_key(b64decode(kdk)),bytes(encryptedCK))
+        print(ck)
+        return ck
+        
+    def decodeContent(self,encryptedPayload,ck,iv):
+        aes_dec = AES.new(ck, AES.MODE_CFB, iv=iv)
+        msg = aes_dec.decrypt(encryptedPayload)
+        print("the message is: ")
+        print(msg, type(msg), str(msg))
+        txt = msg.decode()
+        print(txt)
+        return txt
+    
+                    
+                    
+            
+        
     
--- ex_decryptor.py
+++ ex_decryptor.py
@@ -27,143 +27,17 @@ app = NDNApp()
 
         
 async def main():
-    try:
-        basedir = os.path.dirname(os.path.abspath(sys.argv[0]))
-        tpm_path = os.path.join(basedir, 'privKeys')
-        pib_path = os.path.join(basedir, 'pib.db')
-        keychain = KeychainSqlite3(pib_path, TpmFile(tpm_path))
-        
-        
-        dec = Decryptor(amPrefix)
+    basedir = os.path.dirname(os.path.abspath(sys.argv[0]))
+    tpm_path = os.path.join(basedir, 'privKeys')
+    pib_path = os.path.join(basedir, 'pib.db')
+    keychain = KeychainSqlite3(pib_path, TpmFile(tpm_path))
     
-        contentName = '/Home/livingroom/camera/feed/1'
-        identity = '/Home/user/Alice/KEY'
-        id = keychain.touch_identity(identity)
-        
-        privKeyLoc = sha256(Name.to_bytes(id.default_key().name)).digest().hex() + '.privkey'
-        f = open('privkeys/'+privKeyLoc, "rb")
-        privKey = f.read()
-        print(privKey)
-        print(type(privKey))
-        #print(type(privKey.encode()))
-        
-        name = Name.from_str(contentName)
-        print(f'Sending Interest {Name.to_str(name)}, {InterestParam(must_be_fresh=True, lifetime=6000)}')
-        data_name, meta_info, encryptedContent = await app.express_interest(
-            name, must_be_fresh=True, can_be_prefix=False, lifetime=6000)
-            
-        print(encryptedContent)
-        encryptedModel = EncryptedContent.parse(encryptedContent)
-        print(bytes(encryptedModel.inner.encryptedPayload),type(encryptedModel),bytes(encryptedModel.inner.initializationVector))
-        encryptedPayload = bytes(encryptedModel.inner.encryptedPayload)
-        iv = bytes(encryptedModel.inner.initializationVector)
-        
-        
-        
-        ckName = '/Home/livingroom/camera/feed/1/CK'
-        modCKName = 'Home/livingroom/CK'
-        name = Name.from_str(ckName)
-        print(f'Sending Interest {Name.to_str(name)}, {InterestParam(must_be_fresh=True, lifetime=6000)}')
-        data_name, meta_info, ckData = await app.express_interest(
-            name, must_be_fresh=True, can_be_prefix=False, lifetime=6000)
-            
-        print(bytes(ckData))
-        kekNames = dec.parseCKNames(ckData)
-        #print(kekNames)
-        
-        for kekName in kekNames:
-            kdkName = kekName[:-4]+'/KDK/ENCRYPTED-BY'+ identity
-            print(kdkName)
-        
-        ckNames = dec.buildCKName(ckName,kekNames)
-        print(ckNames)
-        
-        '''name = Name.from_str('/Home/livingroom/CK/ENCRYPTED-BY/Alice/Home/NAC/Home/livingroom/KEK')
-        print(f'Sending Interest {Name.to_str(name)}, {InterestParam(must_be_fresh=True, lifetime=18000)}')
-        data_name, meta_info, encryptedCK = await app.express_interest(
-            name, must_be_fresh=False, can_be_prefix=False, lifetime=18000)
-            
-        if(encryptedCK):
-            print('hola')
-            print(bytes(encryptedCK))'''
-        
-        for key,value in ckNames.items():
-            try:
-                encryptedCK = ''
-                name = Name.from_str(value)
-                print(f'Sending Interest {Name.to_str(name)}, {InterestParam(must_be_fresh=True, lifetime=6000)}')
-                data_name, meta_info, encryptedCK = await app.express_interest(
-                    name, must_be_fresh=False, can_be_prefix=False, lifetime=6000)
-                    
-                #if(encryptedCK):
-                print(encryptedCK)
-                #print(str(name), key)
-                key = Name.normalize(key)
-                print(key)
-                kdkName = Name.to_str(key[:-3])+'/KDK/ENCRYPTED-BY'+ identity
-                
-                print(kdkName)
-                
-                try:
-                    name = Name.from_str(kdkName)
-                    print(f'Sending Interest {Name.to_str(name)}, {InterestParam(must_be_fresh=True, lifetime=6000)}')
-                    data_name, meta_info, kdk = await app.express_interest(
-                    name, must_be_fresh=False, can_be_prefix=False, lifetime=6000)
-                    
-                    print(bytes(kdk))
-                    #print(bytes(privKey))
-                    #kdk = load_priv_key(bytes(kdk))
-                    #print(kdk)
-                    k = ECC.import_key(b64decode(privKey))
-                    kdk = decrypt(k,bytes(kdk))
-                    print(kdk)
-                    
-                    
-                    ck = decrypt(ECC.import_key(b64decode(kdk)),bytes(encryptedCK))
-                    print(ck)
-                    
-                    
-                    aes_dec = AES.new(ck, AES.MODE_CFB, iv=iv)
-                    msg = aes_dec.decrypt(encryptedPayload)
-                    print("the message is: ")
-                    print(msg, type(msg), str(msg))
-                    txt = msg.decode('utf-8')
-                    print(txt)
-                
-                except InterestNack as e:
-                    print(f'Nacked with reason={e.reason}')
-                except InterestTimeout:
-                    print(f'Timeout')
-                except InterestCanceled:
-                    print(f'Canceled')
-                except ValidationFailure:
-                    print(f'Data failed to validate')
-                    
-                    
-            except InterestNack as e:
-                print(f'Nacked with reason={e.reason}')
-            except InterestTimeout:
-                print(f'Timeout')
-            except InterestCanceled:
-                print(f'Canceled')
-            except ValidationFailure:
-                print(f'Data failed to validate')
-                
-        
-        
-        
-        
-        
-    except InterestNack as e:
-        print(f'Nacked with reason={e.reason}')
-    except InterestTimeout:
-        print(f'Timeout')
-    except InterestCanceled:
-        print(f'Canceled')
-    except ValidationFailure:
-        print(f'Data failed to validate')
-    #finally:
-        #app.shutdown()
+    
+    dec = Decryptor(amPrefix)
+
+    contentName = '/Home/livingroom/camera/feed/1'
+    identity = '/Home/user/Alice/KEY'
+    await dec.decode(app,keychain,contentName,identity)
 
 
 if __name__ == '__main__':
